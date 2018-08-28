67832c34d8ca6045075e4544be50d21cd166738d
Andrei Catalin Coman <andreic.coman@gmail.com>
2018-08-28 15:02:34 +0200

    lowercased text before sequence of characters creation. Added new characters and unknown character set to 0

--- utils/converter.py
+++ utils/converter.py
@@ -3,8 +3,7 @@ import numpy as np
 
 class Converter:
     def __init__(self, sequence_max_length=1024):
-        #self.alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:’"/|_#$%ˆ&*˜‘+=<>()[]{} '
-        self.alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:’"/|_#$%ˆ&*˜‘+=<>()[]{} àòèéù'
+        self.alphabet = 'aàbcdeèéfghiìjklmnoòpqrstuùvwxyz0123456789-,;.!?:’\'"/|_#@$%ˆ&*˜‘+=<>()[]{}'
         self.char_dict = {}
         self.sequence_max_length = sequence_max_length
         for i, c in enumerate(self.alphabet):
@@ -18,12 +17,13 @@ class Converter:
             elif text[i] in self.char_dict:
                 data[i] = self.char_dict[text[i]]
             else:
-                # unknown character set to be 68
-                data[i] = 68
+                # unknown character set to be 0
+                data[i] = 0
+            print("{} = {}".format(text[i], str(data[i])))
         return data
 
     def texts_to_sequences(self, dataset):
         D = np.zeros((len(dataset), self.sequence_max_length))
         for dataset_index, dataset_text in enumerate(dataset):
-            D[dataset_index] = self.char2vec(dataset_text)
+            D[dataset_index] = self.char2vec(dataset_text.lower())
         return D
