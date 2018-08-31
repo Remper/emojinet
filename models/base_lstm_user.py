9078fd1913b9b8e9b32c90f16fe074e8323de931
Andrei Catalin Coman <andreic.coman@gmail.com>
2018-08-31 21:30:35 +0200

    added attention

--- models/base_lstm_user.py
+++ models/base_lstm_user.py
@@ -1,10 +1,13 @@
 from keras import Model, Input
-from keras.layers import Dense, Dropout, Embedding, Bidirectional, LSTM, regularizers, Concatenate, Flatten, Activation, RepeatVector, Permute, Multiply
+from keras.layers import Dense, Dropout, Embedding, Bidirectional, LSTM, regularizers, Concatenate, Flatten, Activation, RepeatVector, Permute, Multiply, Lambda
 import numpy as np
+import keras.backend as K
 from keras.optimizers import Adam
 
 
 def base_lstm_user(vocabulary_size: int, embedding_size: int, history_size: int, max_seq_length: int, embedding_matrix: np.array, y_dictionary: dict) -> Model:
+    lstm_units = 256
+
     input = Input(shape=(max_seq_length,), name='main_input')
     history = Input(shape=(history_size,), name='history_input')
 
@@ -15,12 +18,13 @@ def base_lstm_user(vocabulary_size: int, embedding_size: int, history_size: int,
                         trainable=True,
                         embeddings_regularizer=regularizers.l2(0.000001))(input)
     model = Dropout(0.4)(model)
-    model = Bidirectional(LSTM(256, return_sequences=True))(model)
+    model = Bidirectional(LSTM(lstm_units, return_sequences=True))(model)
 
-    attention = Dense(1, activation='tanh', kernel_regularizer=regularizers.l2(0.00001))(model)
-    attention = Flatten()(attention)
+    attention = Dense(1, activation='tanh')(model)
+    #attention = Flatten()(attention)
     attention = Activation('softmax')(attention)
-    attention = RepeatVector(512)(attention)
+    attention = Lambda(lambda x: K.mean(x, axis=1))(attention)
+    attention = RepeatVector(2*lstm_units)(attention)
     attention = Permute([2, 1])(attention)
 
     model = Multiply()([model, attention])
