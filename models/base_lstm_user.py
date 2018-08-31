aa18fc1068f6b34e3572014002aac19e10859c50
Andrei Catalin Coman <andreic.coman@gmail.com>
2018-08-31 16:09:15 +0200

    added attention

--- models/base_lstm_user.py
+++ models/base_lstm_user.py
@@ -1,5 +1,5 @@
 from keras import Model, Input
-from keras.layers import Dense, Dropout, Embedding, Bidirectional, LSTM, regularizers, Average, K, Lambda, Concatenate
+from keras.layers import Dense, Dropout, Embedding, Bidirectional, LSTM, regularizers, Average, K, Lambda, Concatenate, Flatten, Activation, RepeatVector, Permute, Multiply
 import numpy as np
 from keras.optimizers import Adam
 
@@ -16,6 +16,14 @@ def base_lstm_user(vocabulary_size: int, embedding_size: int, history_size: int,
                         embeddings_regularizer=regularizers.l2(0.000001))(input)
     model = Dropout(0.4)(model)
     model = Bidirectional(LSTM(256))(model)
+
+    attention = Dense(1, activation='tanh')(model)
+    attention = Flatten()(attention)
+    attention = Activation('softmax')(attention)
+    attention = RepeatVector(512)(attention)
+    attention = Permute([2, 1])(attention)
+
+    model = Multiply()([model, attention])
 
     h_model = history
     for i in range(2):
