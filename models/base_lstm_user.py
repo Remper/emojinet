80deecdb6abd38d3fbd3f9ac683c56d41dcc7d4b
Andrei Catalin Coman <andreic.coman@gmail.com>
2018-08-31 14:52:12 +0200

    removed attention

--- models/base_lstm_user.py
+++ models/base_lstm_user.py
@@ -1,13 +1,10 @@
 from keras import Model, Input
-from keras.layers import Dense, Dropout, Embedding, Bidirectional, LSTM, regularizers, Average, K, Lambda, Concatenate, Flatten, Activation, RepeatVector, Permute, Multiply
+from keras.layers import Dense, Dropout, Embedding, Bidirectional, LSTM, regularizers, Average, K, Lambda, Concatenate
 import numpy as np
 from keras.optimizers import Adam
 
 
 def base_lstm_user(vocabulary_size: int, embedding_size: int, history_size: int, max_seq_length: int, embedding_matrix: np.array, y_dictionary: dict) -> Model:
-    units = 256
-    activation_units = 2 * units #because of Bidirectional
-
     input = Input(shape=(max_seq_length,), name='main_input')
     history = Input(shape=(history_size,), name='history_input')
 
@@ -18,16 +15,7 @@ def base_lstm_user(vocabulary_size: int, embedding_size: int, history_size: int,
                         trainable=True,
                         embeddings_regularizer=regularizers.l2(0.000001))(input)
     model = Dropout(0.4)(model)
-    model = Bidirectional(LSTM(units, return_sequences = True))(model)
-
-    # compute importance for each step
-    attention = Dense(1, activation='tanh')(model)
-    attention = Flatten()(attention)
-    attention = Activation('softmax')(attention)
-    attention = RepeatVector(activation_units)(attention)
-    attention = Permute([2, 1])(attention)
-
-    model = Multiply([model, attention])
+    model = Bidirectional(LSTM(256))(model)
 
     h_model = history
     for i in range(2):
