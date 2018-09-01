a8446439596ee38f28d1dd4f2a28e7200d87609b
Andrei Catalin Coman <andreic.coman@gmail.com>
2018-09-01 08:50:05 +0200

    restored file previous attention

--- models/base_lstm_user.py
+++ models/base_lstm_user.py
@@ -1,13 +1,10 @@
 from keras import Model, Input
-from keras.layers import Dense, Dropout, Embedding, Bidirectional, LSTM, regularizers, Concatenate, Flatten, Activation, RepeatVector, Permute, Multiply, Lambda, TimeDistributed
+from keras.layers import Dense, Dropout, Embedding, Bidirectional, LSTM, regularizers, Average, K, Lambda, Concatenate
 import numpy as np
-import keras.backend as K
 from keras.optimizers import Adam
 
 
 def base_lstm_user(vocabulary_size: int, embedding_size: int, history_size: int, max_seq_length: int, embedding_matrix: np.array, y_dictionary: dict) -> Model:
-    lstm_units = 256
-
     input = Input(shape=(max_seq_length,), name='main_input')
     history = Input(shape=(history_size,), name='history_input')
 
@@ -18,15 +15,7 @@ def base_lstm_user(vocabulary_size: int, embedding_size: int, history_size: int,
                         trainable=True,
                         embeddings_regularizer=regularizers.l2(0.000001))(input)
     model = Dropout(0.4)(model)
-    model = Bidirectional(LSTM(lstm_units, return_sequences=True))(model)
-
-    attention = TimeDistributed(Dense(1, activation='tanh'))(model)
-    attention = Flatten()(attention)
-    attention = Activation('softmax')(attention)
-    attention = RepeatVector(2*lstm_units)(attention)
-    attention = Permute([2, 1])(attention)
-    attention = Multiply()([model, attention])
-    model = Lambda(lambda xin: K.sum(xin, axis=-1))(attention)
+    model = Bidirectional(LSTM(256))(model)
 
     h_model = history
     for i in range(2):
